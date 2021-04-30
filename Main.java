1fa460839be3a5f9fc6e58a1766e583c998f9ec1
ElliNotiQ <zah.illinot@gmail.com>
2021-04-30 21:10:12 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package ALevel;
+import java.time.LocalDateTime;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Illia Zahynai");
+
+        LocalDateTime date1 = LocalDateTime.of(2021, 4, 30, 20, 38);
+        System.out.println(date1);
+
+
+    }
+}
