9a51096b53089e44c8bfbffe98237be9c15e1bcd
ElliNotiQ <83469385+ElliNotiQ@users.noreply.github.com>
2021-05-03 15:56:42 +0300

    Task1

---
+++ Task 1.java
@@ -0,0 +1,33 @@
+package ALevel;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main {
+//Задание 1 ДЗ(1)
+    /*public static void main(String[] args) {
+        System.out.println("Illia Zahynai");
+
+        LocalDateTime date1 = LocalDateTime.of(2021, 4, 30, 20, 38);
+        System.out.println(date1);
+
+
+
+    }*/
+    //Задание 2 ДЗ(2)
+    public static void main(String[]Object) {
+        ;
+
+        Scanner number = new Scanner(System.in);
+        int a;
+        a = number.nextInt();
+    }
+
+        public static void number.nextInt(int){
+            if (a % 2 == 0)
+                System.out.println("четное");
+            else
+                System.out.println("не четное");
+        }
+
+    }
+
